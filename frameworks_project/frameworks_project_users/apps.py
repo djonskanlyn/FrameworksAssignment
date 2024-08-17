ba290461b04148fab2abb9e309d6cf567c0d29c9
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-08-17 21:03:22 +0100

    1 through 6

---
+++ frameworks_project/frameworks_project_users/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FrameworksProjectUsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'frameworks_project_users'
