fb24e2ac95aa7f55f41743e85f560635c6ce2ee1
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-08-19 21:26:18 +0100

    hide images

--- frameworks_project/frameworks_project/settings.py
+++ frameworks_project/frameworks_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('FRAMEWORKS_PROJECT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['frameworksassignment.onrender.com', 'localhost', '127.0.0.1']
 
