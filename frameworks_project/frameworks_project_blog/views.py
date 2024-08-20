8a2ef29b42cedf35bf944a56352da71a5b875a5f
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-08-20 11:19:51 +0100

    04 Tuesday

--- frameworks_project/frameworks_project_blog/views.py
+++ frameworks_project/frameworks_project_blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
