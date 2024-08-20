bb11b327db634794a1fe7f911d214735e850bdcd
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-08-20 13:01:56 +0100

    07 Tuesday

--- frameworks_project/frameworks_project/settings.py
+++ frameworks_project/frameworks_project/settings.py
@@ -163,6 +163,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('FRAMEWORKS_PROJECT_EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('FRAMEWORKS_PROJECT_EMAIL_PASS')
 DEFAULT_FROM_EMAIL = 'johnscanlon.test.email@gmail.com'
