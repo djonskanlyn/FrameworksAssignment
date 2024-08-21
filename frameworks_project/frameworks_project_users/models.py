2294c149756f72bdbf2a5abb241d37e38dc6c48c
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-08-21 20:19:39 +0100

    fix nesting profile_pics issue

--- frameworks_project/frameworks_project_users/models.py
+++ frameworks_project/frameworks_project_users/models.py
@@ -12,27 +12,30 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
     
     def save(self, *args, **kwargs):
-        # Open the uploaded image
-        img = Image.open(self.image)
-
-        # Define the max width and height for resizing
-        max_size = (300, 300)  # You can change this size to your preference
-
-        # Resize the image while maintaining aspect ratio
-        img.thumbnail(max_size)
-
-        # Save the image to an in-memory file
-        img_io = BytesIO()
-        img_format = self.image.name.split('.')[-1].upper()  # Get the format from the filename
-        if img_format in ["JPG", "JPEG"]:
-            img_format = "JPEG"
-        img.save(img_io, format=img_format)
-
-        # Create a new Django ContentFile and overwrite the original image with the resized one
-        resized_image = ContentFile(img_io.getvalue(), name=self.image.name)
-
-        # Save the resized image to storage (this will automatically upload it to S3)
-        self.image = resized_image
-
-        # Call the original save() method to continue saving the instance
         super().save(*args, **kwargs)
+    
+    # def save(self, *args, **kwargs):
+    #     # Open the uploaded image
+    #     img = Image.open(self.image)
+
+    #     # Define the max width and height for resizing
+    #     max_size = (300, 300)  # You can change this size to your preference
+
+    #     # Resize the image while maintaining aspect ratio
+    #     img.thumbnail(max_size)
+
+    #     # Save the image to an in-memory file
+    #     img_io = BytesIO()
+    #     img_format = self.image.name.split('.')[-1].upper()  # Get the format from the filename
+    #     if img_format in ["JPG", "JPEG"]:
+    #         img_format = "JPEG"
+    #     img.save(img_io, format=img_format)
+
+    #     # Create a new Django ContentFile and overwrite the original image with the resized one
+    #     resized_image = ContentFile(img_io.getvalue(), name=self.image.name)
+
+    #     # Save the resized image to storage (this will automatically upload it to S3)
+    #     self.image = resized_image
+
+    #     # Call the original save() method to continue saving the instance
+    #     super().save(*args, **kwargs)
