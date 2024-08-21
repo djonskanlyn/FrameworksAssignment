e2b08d2d909a35583568f3766b6ed196ddf88b45
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-08-21 20:38:00 +0100

    try agin to save as smaller size

--- frameworks_project/frameworks_project_users/models.py
+++ frameworks_project/frameworks_project_users/models.py
@@ -11,31 +11,29 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-    
     # def save(self, *args, **kwargs):
-    #     # Open the uploaded image
-    #     img = Image.open(self.image)
-
-    #     # Define the max width and height for resizing
-    #     max_size = (300, 300)  # You can change this size to your preference
-
-    #     # Resize the image while maintaining aspect ratio
-    #     img.thumbnail(max_size)
-
-    #     # Save the image to an in-memory file
-    #     img_io = BytesIO()
-    #     img_format = self.image.name.split('.')[-1].upper()  # Get the format from the filename
-    #     if img_format in ["JPG", "JPEG"]:
-    #         img_format = "JPEG"
-    #     img.save(img_io, format=img_format)
-
-    #     # Create a new Django ContentFile and overwrite the original image with the resized one
-    #     resized_image = ContentFile(img_io.getvalue(), name=self.image.name)
-
-    #     # Save the resized image to storage (this will automatically upload it to S3)
-    #     self.image = resized_image
-
-    #     # Call the original save() method to continue saving the instance
     #     super().save(*args, **kwargs)
+    
+    def save(self, *args, **kwargs):
+        # Save the model first to ensure we have access to the image
+        super().save(*args, **kwargs)
+
+        # Check if an image exists
+        if self.image:
+            img = Image.open(self.image)  # Open the uploaded image file
+
+            # Resize the image if necessary (e.g., if it's larger than 300x300)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)  # Resize the image while maintaining aspect ratio
+
+                # Save the resized image to a BytesIO object (in memory)
+                img_io = BytesIO()
+                img_format = img.format if img.format else 'JPEG'  # Get the original format (e.g., JPEG, PNG)
+                img.save(img_io, format=img_format)
+
+                # Replace the image field with the resized image
+                self.image.save(self.image.name, ContentFile(img_io.getvalue()), save=False)
+
+        # Call save again to save the resized image
+        super().save(*args, **kwargs)
