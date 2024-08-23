6c147d4d88a999e1b1a9292c56daf31804582107
djonskanlyn <39839823+djonskanlyn@users.noreply.github.com>
2024-08-23 22:33:10 +0100

    Final Friday

--- frameworks_project/frameworks_project_recipes/models.py
+++ frameworks_project/frameworks_project_recipes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    recipe = models.CharField(max_length=250)
+    category = models.CharField(max_length=250)
+    region = models.CharField(max_length=250)
+    instructions = models.TextField()
+    image = models.CharField(max_length=250, blank=True, null=True)
+    youtube = models.CharField(max_length=250, blank=True, null=True)
+    api_id = models.CharField(max_length=20, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+            return self.recipe
+
+class Ingredient(models.Model):
+    ingredient = models.CharField(max_length=250)
+    measure = models.CharField(max_length=250)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ingredient
